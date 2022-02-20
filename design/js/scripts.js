aaaf9a342264460d2b934ec6ceccd835e435ed5e
ousmane <omdousmane@gmail.com>
2022-02-20 19:51:32 +0100

    dernier commit

--- design/js/scripts.js
+++ design/js/scripts.js
@@ -1,28 +1,17 @@
-let headerDesktop = document.querySelector(".header-desktop");
-let headerMobile = document.querySelector(".header-menu__mobile");
-console.log(headerMobile);
-let ticking = false;
-
-window.addEventListener("scroll", function (e) {
-  scrollPosition = window.scrollY;
-  if (scrollPosition <= 1000 || scrollPosition >= 2200) {
-    headerDesktop.classList.add("slide-bottom");
-    headerDesktop.classList.remove("slide-top");
-    headerDesktop.style.display = "flex";
-  } else {
-    headerDesktop.classList.add("slide-top");
-    headerDesktop.classList.remove("slide-bottom");
-    headerDesktop.style.display = "none";
-  }
-  if (screen.width <= 760) {
-    headerDesktop.style.display = "none";
-    headerDesktop.classList.add("slide-bottom");
-  }
-  if (!ticking) {
-    window.requestAnimationFrame(function () {
-      ticking = false;
-    });
-  }
-
-  ticking = true;
+window.addEventListener("scroll", () => {
+  (function scroll() {
+    const header = document.getElementById("autofixed");
+    if (header) {
+      if (window.pageYOffset > 800) {
+        header.classList.add("header-animated");
+      } else {
+        header.classList.remove("header-animated");
+      }
+      if (window.pageYOffset > 1200) {
+        header.classList.add("fixed-top");
+      } else {
+        header.classList.remove("fixed-top");
+      }
+    }
+  })();
 });
